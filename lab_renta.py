2fac03ed92baf600f562f90651c1dfdff4496080
Samara Albertos <salbbla@canariaseducacion.es>
2026-02-19 17:52:29 +0000

    Añadimos los asset al fichero python y ejecutamos

--- lab_renta.py
+++ lab_renta.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from dagster import asset
-from plotnine import ggplot,aes, geom_col
+import os
+from dagster import  asset, AssetExecutionContext
+from plotnine import ggplot , aes, geom_col, theme_minimal, labs, theme, element_text
 
 
 @asset
@@ -20,17 +21,127 @@ def limpiar_datos(leer_datos:pd.DataFrame):
     })
     df["anio"] = df["anio"].astype(int)
     df["valor"] = pd.to_numeric(df["valor"], errors="coerce")
+
+    df = df.dropna(subset=["anio", "valor", "territorio", "medida"])
+
+    
+    df["territorio"] = df["territorio"].astype(str).str.strip().str.lower()
+    df["medida"] = df["medida"].astype(str).str.strip().str.lower()
+
     return df
 
 @asset
-def mostrar_datos(limpiar_datos:pd.DataFrame):
-    df = limpiar_datos
-    print (df)
+def renta_2022(limpiar_datos: pd.DataFrame):
+    return limpiar_datos[limpiar_datos["anio"] == 2022].copy()
+
+@asset 
+def plot_renta_canarias_2022 (context: AssetExecutionContext, renta_2022: pd.DataFrame):
+    p =(
+        ggplot(renta_2022, aes(
+        x="medida",
+        y="valor"
+        ))
+        + geom_col()
+        + theme_minimal()
+        + labs(
+            title="Distribución de renta en Canarias",
+             subtitle="Año 2022",
+             x="Tramo de renta",
+            y="Valor",
+            caption="Fuente: Instituto de Estadística de Canarias"
+        )
+    )
+  
+    os.makedirs("outputs", exist_ok=True)
+
+    p.save("outputs/grafico_renta_Canarias.png", dpi=150, width=12, height=6)
+    print("Gráfico guardado en outputs/grafico_renta_Canarias.png")
+
+
+
 
 @asset
 def leer_codislas():
-    return pd.read_csv("codislas.csv")
+    codislas =pd.read_csv("codislas.csv", encoding="cp1252", sep=";")
+    if "ISLA" in codislas.columns:
+        codislas["ISLA"] = codislas["ISLA"].astype(str).str.strip().str.lower()
+    if "NOMBRE" in codislas.columns:
+        codislas["NOMBRE"] = codislas["NOMBRE"].astype(str).str.strip()
+
+    return codislas
 
 @asset
-def unir_datos(limpiar_datos: pd.DataFrame, leer_codilsas: pd.DataFrame):
-    
+def unir_datos(renta_2022: pd.DataFrame, leer_codislas: pd.DataFrame):
+    df= renta_2022.merge(
+        leer_codislas,
+        left_on="territorio",
+        right_on="ISLA",
+        how="inner"
+    )
+    return df
+
+
+
+@asset
+def plot_renta_municipios_2022(context: AssetExecutionContext, unir_datos: pd.DataFrame):
+    p = (
+        ggplot(unir_datos, aes(x="medida", y="valor", fill="NOMBRE"))
+        + geom_col(position="dodge")
+        + theme_minimal()
+        + theme(
+            legend_position="bottom",
+            legend_title=element_text(size=8),
+            legend_text=element_text(size=6),
+            legend_key_size=6,
+        )
+        + labs(
+            title="Distribución de renta en Canarias por municipios",
+            subtitle="Año 2022",
+            x="Tramo de renta",
+            y="Valor",
+            fill="Municipio",
+            caption="Fuente: Instituto de Estadística de Canarias",
+        )
+    )
+
+    os.makedirs("outputs", exist_ok=True)
+
+    p.save("outputs/grafico_renta_CanariasMunicipio.png", dpi=150, width=12, height=6)
+    print("Gráfico guardado en outputs/grafico_renta_CanariasMunicipio.png")
+
+
+@asset
+def renta_pensiones_2022(unir_datos: pd.DataFrame):
+    # Filtra solo la medida "Pensiones"
+    df = unir_datos[unir_datos["medida"] == "Pensiones"].copy()
+    return df
+
+
+@asset
+def plot_pensiones_municipios_2022(context: AssetExecutionContext, renta_pensiones_2022: pd.DataFrame):
+
+    p = (
+        ggplot(renta_pensiones_2022, aes(x="medida", y="valor", fill="NOMBRE"))
+        + geom_col(position="dodge")
+        + theme_minimal()
+        + theme(
+            legend_position="bottom",
+            legend_title=element_text(size=8),
+            legend_text=element_text(size=6),
+            legend_key_size=6,
+        )
+        + labs(
+            title="Distribución de pensiones en Canarias por municipios",
+            subtitle="Año 2022",
+            x="Pensiones",
+            y="Valor",
+            fill="Municipio",
+            caption="Fuente: Instituto de Estadística de Canarias",
+        )
+    )
+
+    os.makedirs("outputs", exist_ok=True)
+
+    p.save("outputs/grafico_CanariasMunicipioPensiones.png", dpi=150, width=12, height=6)
+    print("Gráfico guardado en outputs/grafico_renta_CanariasMunicipioPensiones.png")
+
